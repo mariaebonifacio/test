da9350ab9210117a4f3ec8252ad34bef65a69a9f
mariaebonifacio <madubonifacio12@gmail.com>
2025-08-26 09:39:52 -0300

    testes feitos (erro)

--- tests/test_juros_composto.py
+++ tests/test_juros_composto.py
@@ -2,65 +2,71 @@ import pytest
 from juros_composto import calcular_juros_compostos
 
 
-# VALIDANDO CAPITAL ------------------------------------------------------
-    # Validação do capital investido como string
-def test_validar_capital_string():
+# ------------------------------------VALIDANDO STRING ------------------------------------------------------
+# capital
+def test_validar_capital():
     # Definindo a entrada
-    capital = 1000
+    capital = "oi"
     taxa = 10
     tempo = 3
 
     # Executando a função e esperando erro
     with pytest.raises(ValueError, match="O capital investido deve ser um número (int ou float)."):
         calcular_juros_compostos(capital, taxa, tempo)
+# TAXA
+def test_validar_taxa():
+    #define entrada
+    capital = 1000
+    taxa = "oi"
+    tempo = 3
 
-    # Validação do capital investido como negativo
+    #executando a função e esperando erro
+    with pytest.raises(ValueError, match="A taxa de juros deve ser um número (int ou float)."):
+        calcular_juros_compostos(capital, taxa, tempo)
+
+# TEMPO
+def test_validar_tempo():
+    #Definindo a entrada
+    capital = 1000
+    taxa = 10
+    tempo = "oi"
+
+    #executando a função e esperando o erro
+    with pytest.raises(ValueError, match="O tempo deve ser um número (int ou float)."):
+        calcular_juros_compostos(capital, taxa, tempo)
+
+# ------------------------------------VALIDANDO NEGATIVO ----------------------------------------------------
+# CAPITAL
 def test_validar_capital_negativo():
     # Definindo a entrada
     capital = -1000
-    taxa = -10
-    tempo = -3
+    taxa = 10
+    tempo = 3
 
     # Executando a função e esperando erro
     with pytest.raises(ValueError, match="O capital investido não pode ser negativo."):
         calcular_juros_compostos(capital, taxa, tempo)
 
+# TAXA
+def test_validar_taxa_negativo():
+    # Definindo a entrada
+    capital = 1000
+    taxa = -10
+    tempo = 3
 
-# VALIDANDO TAXA DE JUROS----------------------------------------------
-def test_calcular_taxa_juros_nmr():
-    #define entrada
-    entrada = 3
+    # Executando a função e esperando erro
+    with pytest.raises(ValueError, match="O capital investido não pode ser negativo."):
+        calcular_juros_compostos(capital, taxa, tempo)
 
-    #executando a função e esperando erro
-    with pytest.raises(TypeError, match="A taxa de juros deve ser um número (int ou float)."):
-        calcular_juros_compostos(entrada)
-
-
-def test_calcular_taxa_juros_negativo():
-    #define entrada
-    entrada = -3
-
-    #executando a função e esperando erro
-    with pytest.raises(ValueError, match="A taxa de juros não pode ser negativa."):
-        calcular_juros_compostos(entrada)
-
-
-# VALIDANDO TEMPO -------------------------------------------------------
-# Validação do tempo
-def test_validar_tempo():
-    #Definindo a entrada
-    entrada = 10
-
-    #executando a função e esperando o erro
-    with pytest.raises(ValueError, match="O tempo deve ser um número (int ou float)."):
-        calcular_juros_compostos(entrada)
-
-# validando negativo
+# TEMPO
 def test_validar_tempo_negativo():
     # Definindo a entrada
-    entrada = -10
+    capital = 1000
+    taxa = 10
+    tempo = -3
 
     # executando a função e esperando o erro
-    with pytest.raies(TypeError, match="O tempo não pode ser negativo."):
-        calcular_juros_compostos(entrada)
+    with pytest.raies(ValueError, match="O tempo não pode ser negativo."):
+        calcular_juros_compostos(capital, taxa, tempo)
+
 
